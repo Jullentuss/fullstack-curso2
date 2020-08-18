4a30dd1bd8069ee3058705219c0d57331ceb2db0
Julen Rubio <julen.rubio@outlook.com>
2020-08-18 10:33:04 -0500

    stash de rutas

--- wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
+++ wishlist/src/app/form-destino-viaje/form-destino-viaje.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
   selector: 'app-form-destino-viaje',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-destino-viaje.component.css']
 })
 export class FormDestinoViajeComponent implements OnInit {
+  @Output() onItemAdded: EventEmitter<DestinoViaje>;
+  fg: FormGroup;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.onItemAdded = new EventEmitter();
+    this.fg = fb.group({
+      nombre: [''],
+      url: ['']
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+
+  guardar(nombre: string, url: string): boolean {
+    this.onItemAdded.emit(new DestinoViaje(nombre, url));
+    return false;
   }
 
 }
--- wishlist/src/app/lista-destinos/lista-destinos.component.ts
+++ wishlist/src/app/lista-destinos/lista-destinos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { DestinoViaje } from '../models/destino-viaje.model';
 
 @Component({
@@ -7,22 +7,23 @@ import { DestinoViaje } from '../models/destino-viaje.model';
   styleUrls: ['./lista-destinos.component.css']
 })
 export class ListaDestinosComponent implements OnInit {
-  destinos: DestinoViaje[];
+  @Output() onItemAdded: EventEmitter<DestinoViaje>
 
-  constructor() {
-    this.destinos = [];
+
+  constructor( destinosApiClient: DestinosApiClient) {
+    this.onItemAdded = new EventEmitter();
   }
 
   ngOnInit(): void {
   }
 
-  guardar(nombre: string, url: string): boolean {
-    this.destinos.push(new DestinoViaje(nombre, url));
-    return false;
+  agregar(d: DestinoViaje) {
+    this.destinosApiClient(d);
+    this.onItemAdded.emit(d);
   }
 
   elegido(d: DestinoViaje) {
-    this.destinos.forEach(x => x.setSelected(false));
+    this.destinosApiClient.forEach(x => x.setSelected(false));
     d.setSelected(true);
   }
 }
